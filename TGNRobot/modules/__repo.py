3a5396c241d9023cb2c6b17e87c1cf20d7a94427
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-11-07 22:52:23 +0700

    Update __repo.py

--- TGNRobot/modules/__repo.py
+++ TGNRobot/modules/__repo.py
@@ -9,14 +9,14 @@ REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/e1ff8620519d6378ac1f0.jp
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("✯ ʀᴇᴘᴏ ✯", url=f"https://github.com/ridho17-ind"),
-        InlineKeyboardButton("✯ ᴊᴏɪɴ ✯", url=f"https://t.me/FlicksSupport"),
+        InlineKeyboardButton("ʀᴇᴘᴏ", url=f"https://github.com/ridho17-ind"),
+        InlineKeyboardButton("ᴊᴏɪɴ", url=f"https://t.me/FlicksSupport"),
       ],[
-        InlineKeyboardButton("✯ ᴏᴡɴᴇʀ ✯", url="https://t.me/XFLSkyzo"),
-        InlineKeyboardButton("✯ ꜱᴜᴘᴘᴏʀᴛ ✯", url="https://t.me/FlicksSupport"),
+        InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/XFLSkyzo"),
+        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/FlicksSupport"),
       ],[
-        InlineKeyboardButton("✯ ᴜᴘᴅᴀᴛᴇꜱ ✯", url="https://t.me/SadRoomsInfo"),
-        InlineKeyboardButton("✯ ᴅᴇᴠᴇʟᴏᴘᴇʀ ✯", url="https://t.me/devoloperflicks/32"),
+        InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url="https://t.me/SadRoomsInfo"),
+        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/devoloperflicks/32"),
       ]]
     )
   
