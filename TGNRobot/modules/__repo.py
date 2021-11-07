1b9ad45a70842bb7672788f5ded201976ef7cd87
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-11-07 22:03:17 +0700

    Update __repo.py

--- TGNRobot/modules/__repo.py
+++ TGNRobot/modules/__repo.py
@@ -5,18 +5,18 @@ from pyrogram.types import *
 from TGNRobot.conf import get_str_key
 from TGNRobot import pbot
 
-REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/cab6825dea9263d347831.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Akki](t.me/godfatherakki) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @The_Godfather_Network «««"
+REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/e1ff8620519d6378ac1f0.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Akki](t.me/godfatherakki) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @The_Godfather_Network «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("⚡ ʀᴇᴘᴏꜱɪᴛᴏʀʏ🔥", url=f"https://github.com/Itsunknown-12/TGN-Robot"),
-        InlineKeyboardButton(" ᴊᴏɪɴ 💫", url=f"https://t.me/Superior_Bots"),
+        InlineKeyboardButton(" 🔥ʀᴇᴘᴏ 🔥", url=f"https://github.com/ridho17-ind"),
+        InlineKeyboardButton("💫 ᴊᴏɪɴ 💫", url=f"https://t.me/FlicksSupport"),
       ],[
-        InlineKeyboardButton("ᴛɢɴ ᴏᴡɴᴇʀ ❣️", url="https://t.me/godfatherakki"),
-        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ ⚡", url="https://t.me/GodfatherSupport"),
+        InlineKeyboardButton("🔰 ᴏᴡɴᴇʀ 🔰", url="https://t.me/XFLSkyzo"),
+        InlineKeyboardButton("⚡ ꜱᴜᴘᴘᴏʀᴛ ⚡", url="https://t.me/FlicksSupport"),
       ],[
-        InlineKeyboardButton("⚡ ᴜᴘᴅᴀᴛᴇꜱ ☑️", url="https://t.me/The_Godfather_Network"),
-        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ➡️", url="https://t.me/Timesisnotwaiting"),
+        InlineKeyboardButton("🥜 ᴜᴘᴅᴀᴛᴇꜱ 🥜", url="https://t.me/SadRoomsInfo"),
+        InlineKeyboardButton("👩‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ 👩‍💻", url="https://t.me/XFLSkyzo"),
       ]]
     )
   
