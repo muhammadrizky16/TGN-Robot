532d0dfddba1e828963303ea6bf54c49c0f6ebeb
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-09 08:52:49 +0700

    Update __repo.py

--- TGNRobot/modules/__repo.py
+++ TGNRobot/modules/__repo.py
@@ -5,17 +5,17 @@ from pyrogram.types import *
 from TGNRobot.conf import get_str_key
 from TGNRobot import pbot
 
-REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/e1ff8620519d6378ac1f0.jpg) To Make Your Groups Secured And Organized !** \n\n**『 Owner 』 : 『 [Skyzo](t.me/xflskyzo) 』**\n**╭──────────────**\n**┣─ » Python ~ 3.8.6**\n**┣─ » Update ~ Recently**\n**╰──────────────**\n\n**»» 『 @SadRoomsInfo 』 ««**"
+REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/e1ff8620519d6378ac1f0.jpg) To Make Your Groups Secured And Organized !** \n\n**『 Owner 』 : 『 [Kyy](t.me/IDnyaKosog) 』**\n**╭──────────────**\n**┣─ » Python ~ 3.8.6**\n**┣─ » Update ~ Recently**\n**╰──────────────**\n\n**»» 『 @ahhsudahlahhh 』 ««**"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("ʀᴇᴘᴏ", url=f"https://github.com/ridho17-ind"),
+        InlineKeyboardButton("ʀᴇᴘᴏ", url=f"https://github.com/muhammadrizky16"),
         InlineKeyboardButton("ᴊᴏɪɴ", url=f"https://t.me/FlicksSupport"),
       ],[
-        InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/XFLSkyzo"),
+        InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/IDnyaKosong"),
         InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/FlicksSupport"),
       ],[
-        InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url="https://t.me/SadRoomsInfo"),
+        InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url="https://t.me/ahhsudahlahhh"),
         InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/devoloperflicks/32"),
       ]]
     )
